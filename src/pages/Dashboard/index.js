87f5e4dbea6db574fb09be39697cd998e83f45cc
Patrick Silva <patrickeeep12@gmail.com>
2020-06-10 17:20:21 -0300

    First commit meetapp mobile

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Dashboard = () => {
+  return <View />;
+};
+
+export default Dashboard;
